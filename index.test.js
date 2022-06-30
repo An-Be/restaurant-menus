b548fd9af1894e68ac0895ed2f65663ceca9a4bd
Andrea <andrea.berrocal95@gmail.com>
2022-06-30 14:56:36 -0700

    completed tests and models

--- index.test.js
+++ index.test.js
@@ -7,34 +7,40 @@ describe('Restaurant and Menu Models', () => {
    * Runs the code prior to all tests
    */
   beforeAll(async () => {
-    // the 'sync' method will create tables based on the model class
-    // by setting 'force:true' the tables are recreated each time the
-    // test suite is run
-    await sequelize.sync({ force: true });
+      // the 'sync' method will create tables based on the model class
+      // by setting 'force:true' the tables are recreated each time the 
+      // test suite is run
+      await sequelize.sync({ force: true });
   });
 
   test('can create a Restaurant', async () => {
-    // TODO - write test
-    expect('NO TEST').toEqual('EXPECTED DATA');
+      // TODO - write test
+      const restaurant1 = await Restaurant.create(seedRestaurant[0])
+      expect(restaurant1.name).toBe(seedRestaurant[0].name)
   });
 
   test('can create a Menu', async () => {
-    // TODO - write test
-    expect('NO TEST').toEqual('EXPECTED DATA');
+      // TODO - write test
+      const menu1 = await Menu.create(seedMenu[0])
+      expect(menu1.name).toBe(seedMenu[0].name)
   });
 
   test('can find Restaurants', async () => {
-    // TODO - write test
-    expect('NO TEST').toEqual('EXPECTED DATA');
+      // TODO - write test
+      const findRestaurants = await Restaurant.findAll();
+      expect(findRestaurants.length).toEqual(1)
   });
 
   test('can find Menus', async () => {
-    // TODO - write test
-    expect('NO TEST').toEqual('EXPECTED DATA');
+      // TODO - write test
+      const findMenus = await Menu.findAll();
+      expect(findMenus.length).toEqual(1)
   });
 
   test('can delete Restaurants', async () => {
-    // TODO - write test
-    expect('NO TEST').toEqual('EXPECTED DATA');
+      // TODO - write test
+      const restaurant2 = await Restaurant.create(seedRestaurant[0])
+      const deletedRestaurants = await restaurant2.destroy();
+      expect(Restaurant.length).toEqual(0);
   });
-});
+})
